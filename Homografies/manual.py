0416839970c728dde204f31ce2e3d8ec09b17c5e
Miquel <miquelca32@gmail.com>
2020-10-26 13:11:35 +0100

    First commit

---
+++ Homografies/manual.py
@@ -0,0 +1,68 @@
+""" Make an homography between two images.
+
+This script makes the homography between two images. To do so we select manually
+the correspondence set of points.
+
+"""
+import cv2
+from matplotlib import pyplot as plt
+import numpy as np
+
+punts = {}
+
+
+def on_click(name: str, fig, event):
+    """ Select a set of points.
+
+    Select a set of points from Matplotlib. The points are saved on a global
+    dictionary, whom keys are passed as parameter to this funciton. The fig
+    parameter is the figure where to draw the points
+
+    Args:
+        name (str): Key of the dictionary, unique
+        fig (figure): Matplotlib object, figure obtained from plt.figure
+        event : Information frm the event
+
+    Returns:
+        None
+    """
+    if name not in punts:
+        punts[name] = []
+    punts[name].append([event.xdata, event.ydata])
+    plt.scatter(event.xdata, event.ydata)
+    fig.canvas.draw()
+
+
+def run():
+    # We read the images
+    img1 = cv2.imread("./in/llibre1.jpg")
+    img2 = cv2.imread("./in/llibre3.jpg")
+
+    # We show the first image allowing to select the points.
+    fig1 = plt.figure()
+    fig1.canvas.mpl_connect('button_press_event',
+                            lambda x: on_click("img_1", fig1, x))
+    plt.imshow(img1)
+    plt.show()
+
+    # We show the second image allowing to select the points.
+    fig2 = plt.figure()
+    fig2.canvas.mpl_connect('button_press_event',
+                            lambda x: on_click("img_2", fig2, x))
+    plt.imshow(img2)
+    plt.show()
+
+    # We find the homography
+    h, status = cv2.findHomography(np.array(punts["img_1"]),
+                                   np.array(punts["img_2"]))
+
+    # We change the persvective
+    im_out = cv2.warpPerspective(img1, h, (img2.shape[1], img2.shape[0]))
+
+    # We show the image warpped
+    plt.figure()
+    plt.imshow(im_out)
+    plt.show()
+
+
+run()
---
+++ README.md
@@ -0,0 +1,11 @@
+# Visió per computador 2020-2021 - UIB 
+
+Aquest repositori conté tots els exemples de l'assignatura vists a classe. 
++ Homografies
+     + ``manual.py``. Script a on es seleccionen els punts d'interès de forma
+     manual mitjançant l'ús d'una intefície gràfica.   
+     + ``billboard.py``. Emprant la matexa tècnica que ``manual.py`` cream una
+     image composta.
+     + ``autop.py``. Script a on es realitza la homografia entre dues imatges
+        de manera automàtica fent ús de descriptors com ORB.
+ 
